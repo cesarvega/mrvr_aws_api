bb64abbd25627a22bd584b9176cf22353fd31ce7
cesarvega <cesarvega.col@gmail.com>
2022-06-07 11:04:49 -0400

    json format api fixed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const app = require("express")();
-
+const express  = require("express");
+const app      = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+app.use(express.json());
+
+
 
 app.get("/item", (req, res) => {
   res.status(200).send({
@@ -15,11 +15,15 @@ app.get("/item", (req, res) => {
 
 app.post("/item/:id", (req, res) => {
 
-//   const { id } = req.params;
-//   const { logo } = req.body;
+  const { id } = req.params;
+  const { logo } = req.body;
 
-//   res.status(200).send({
-//     id,
-//     logo,
-//   });
+  res.status(200).send({
+    id,
+    logo,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`);
 });
